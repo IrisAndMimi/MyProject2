dacc8bb577623279b7c04fbbd67e6f1c0be83a41
IrisAndMimi <my1ris227@outlook.com>
2018-03-13 14:40:17 +0800

    default commit

--- src/com/java2/object/Restaurant.java
+++ src/com/java2/object/Restaurant.java
@@ -1,7 +1,7 @@
 package com.java2.object;
 
 public class Restaurant {
-<<<<<<< HEAD
+
 	protected int salary;
 	protected float bonus = 0.1f;
 
@@ -12,11 +12,6 @@ public class Restaurant {
 	private void print() {
 		int year = (int) (salary * bonus);
 	}
-=======
-  int salary;
-  int bonus;
-  public void print(){
-	  
-  }
->>>>>>> fd466c0b51181984dbb1e840e068cb4032e86e9c
+
+  
 }
