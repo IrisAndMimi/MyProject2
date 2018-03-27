e4609ebba23e16278179a3ce42cecd74c8bd93ac
IrisAndMimi <my1ris227@outlook.com>
2018-03-27 15:56:19 +0800

    cards.add/size/get/remove 第一項代號0

--- src/com/java2/object/ListTester.java
+++ src/com/java2/object/ListTester.java
@@ -7,10 +7,16 @@ public class ListTester {
 
 	public static void main(String[] args) {
 		List<Integer> cards = new ArrayList<>();
-		cards.add(0);
-		cards.add(1);
-		cards.add(3);
-		System.out.println(cards.size());
+		cards.add(87);
+		cards.add(123);
+		cards.add(22);
+		cards.add(99);
+		System.out.println("本來有"+cards.size()+"個");
+		System.out.println("打算移除"+cards.get(0));
+		cards.remove(0);
+		System.out.println("第2項變成了"+cards.get(1));
+		System.out.println("再來移除第2項:"+cards.remove(1));
+		System.out.println("剩下"+cards.size()+"個");
 	}
 
 }
