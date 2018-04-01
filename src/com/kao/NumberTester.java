78bf43e18995cd9ae52a6fc5fff9bcc51e1b748c
IrisAndMimi <my1ris227@outlook.com>
2018-04-01 12:29:46 +0800

    排版

--- src/com/kao/NumberTester.java
+++ src/com/kao/NumberTester.java
@@ -6,10 +6,12 @@ public class NumberTester {
 	}
 
 	public static void main(String[] args) {
-for(int number =1;number<=100;number++) {
-	if( number%15==0) {
-	System.out.println(number+" ");
-}else {if(number%3==0)System.out.print(number+" ");}
-	}}
+		for(int number =1;number<=100;number++) {
+			if( number%15==0) {
+				System.out.println(number+" ");
+		}else{
+			if(number%3==0)System.out.print(number+" ");}
+		}
+	}
 
 }
